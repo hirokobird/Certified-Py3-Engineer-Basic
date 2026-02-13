71f775c6eb75e2c9ac67e3621bd266beb3482414
Hiroko Tok <hiroko.torimura@gmail.com>
2026-02-13 16:49:36 +0900

    Prime Study試験対策

--- primestudy/ps2_no09.py
+++ primestudy/ps2_no09.py
@@ -1,4 +1,3 @@
-
 # No.9
 
 months = ['January', 'March', 'May', 'July']
@@ -10,16 +9,3 @@ for month in months[:]:
         months.insert(0, month)
 
 print(months, end ='')
-
-print('================')
-
-py = ["P","y","t","h","o","n"]
-print(py[1:3])
-print(py[3])
-print(py[-2:-5])
-
-try:
-    raise TypeError("型が不正です", "入力しなおしてください")
-except TypeError as t:
-    print(t.args)
-
